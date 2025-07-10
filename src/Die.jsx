ca6888d92dd37af4883299bde791e699d08514da
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-07-11 01:40:20 +0300

    Added the Hold functionality to hold a number even when Roll is clicked.

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 export default function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white",
+    };
     return (
-        <div className="die-face">
-            <h2 className="die-numm">{props.value}</h2>
+        <div className="die-face" style={styles} onClick={props.holdDice}>
+            <h2 className="die-num">{props.value}</h2>
         </div>
     );
 }
