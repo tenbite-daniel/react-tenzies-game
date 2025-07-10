6c74bdc05755d638a6831bfbb5898a56d63921e0
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-07-11 01:03:21 +0300

    Added Roll button which generates 10 new random numbers when clicked.

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,19 @@ function App() {
         }
         return newDice;
     }
+
+    function rollDice() {
+        setDice(allNewDice());
+    }
+
     const diceElements = dice.map((die) => <Die value={die} />);
 
     return (
         <main>
             <div className="die-container">{diceElements}</div>
+            <button className="roll-dice" onClick={rollDice}>
+                Roll
+            </button>
         </main>
     );
 }
